fb942271f7b6fefc6880079c55b48599e2df042e
Herman Vasilkouski <hermanvasilkouski@gmail.com>
2026-04-08 19:38:07 +0300

    feat: add docker compose

--- src/user/user-api.module.ts
+++ src/user/user-api.module.ts
@@ -3,9 +3,11 @@ import { UserModule } from './user.module';
 import { DeleteUserUseCase } from './use-cases/delete-user.use-case';
 import { ArticleModule } from 'src/article/article.module';
 import { CommentModule } from 'src/comment/comment.module';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [UserModule, ArticleModule, CommentModule],
+  controllers: [UserController],
   providers: [DeleteUserUseCase],
 })
 export class UserApiModule {}
